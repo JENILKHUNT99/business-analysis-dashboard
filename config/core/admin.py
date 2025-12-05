77dda1351517ea46f338f4442ffa2eb901f54211
JENILKHUNT99 <jenilkhunt22222@gmail.com>
2025-12-06 00:24:11 +0530

    models are registerd in core\admin.py file..

--- config/core/admin.py
+++ config/core/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Product, Customer, Order, OrderItem, Expense
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'sku', 'category', 'stock', 'is_low_stock')
+    list_filter = ('category',)
+    search_fields = ('name', 'sku')
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone', 'email', 'city', 'created_at')
+    search_fields = ('name', 'phone', 'email')
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer', 'order_date', 'payment_method', 'total_amount')
+    list_filter = ('payment_method', 'order_date')
+    inlines = [OrderItemInline]
+
+
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('category', 'amount', 'date', 'created_at')
+    list_filter = ('category', 'date')
